11a756bbf6d148d6c39e4cbfe9459c0a018650ef
SokolovaAnastasiia <aasokolova99@gmail.com>
2021-10-21 20:56:44 +0300

    Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+      resources :comments
+  end
   get 'post/index'
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
