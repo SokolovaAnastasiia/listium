85128fed2833a21b9ac51a4692d478cb3472a6ed
SokolovaAnastasiia <aasokolova99@gmail.com>
2021-10-22 07:53:24 +0300

    Promo page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+posts = [
+{
+	name: "1",
+	title: "The Bad Seeds",
+	content: "Nick Cave",
+	link: "Tracklist"
+},
+{
+	name: "2",
+	title: "The Bad Seeds Old",
+	content: "Nick Cave Old Too",
+	link: "Some Old Tracklist"
+},
+{
+    name: "3",
+    title: "The Bad Seeds new",
+    content: "Nick Cave Old Too",
+    link: "Some Old Tracklist"
+}
+]
+
+
+posts.each do |post|
+	postik = Post.create(post)
+	puts "Some magic just create a #{ postik.name } with id #{ postik.id }!"
+end
