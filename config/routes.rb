d25172be927c4f2b9a6060a5f4748e8a87831550
SokolovaAnastasiia <aasokolova99@gmail.com>
2021-12-20 18:20:32 +0300

    Профили пользователей

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   get 'promo', to: 'promo#index'
   get 'about', to: 'about#index'
 
+  get 'users', to: 'users#index'
+  get 'users/:id' => 'users#show', :as => :user
+  delete 'users/:id', to: 'users#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'promo#index'
 end
