f92d62c9cf54ba91ad415482f48da803c9658fe9
SokolovaAnastasiia <aasokolova99@gmail.com>
2021-12-20 18:58:24 +0300

    Подписки на пользователей и админов

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :follows
   resources :categories
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users
@@ -10,13 +11,20 @@ Rails.application.routes.draw do
     resources :likes
     resources :favorites
   end
+
+  resources :users
+  post 'users/:id/follow', to: "users#follow", as: "follow_user"
+  post 'users/:id/unfollow', to: "users#unfollow", as: "unfollow_user"
+
+  get 'users/:id/following', :to => "users#following", :as => :following
+
   get 'post/index'
   get 'welcome/index'
   get 'promo', to: 'promo#index'
   get 'about', to: 'about#index'
 
   get 'users', to: 'users#index'
-  get 'users/:id' => 'users#show', :as => :user
+  get 'users/:id' => 'users#show'
   delete 'users/:id', to: 'users#destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
